aa5f5acf2033a485de0947c4219afd75888b0281
Abdoul Aziz BA <aaziz.ba2@univ-thies.sn>
2022-03-30 23:27:28 +0000

    Refactoring last commit

--- src/app/reservation-page/reservation-page.module.ts
+++ src/app/reservation-page/reservation-page.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReservationComponent } from './components/reservation/reservation.component';
-import { AddReservationComponent } from './components/add-reservation/add-reservation.component';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [ReservationComponent, AddReservationComponent],
+  declarations: [ReservationComponent],
   imports: [CommonModule, RouterModule, ReactiveFormsModule],
   exports: [ReservationComponent],
 })
